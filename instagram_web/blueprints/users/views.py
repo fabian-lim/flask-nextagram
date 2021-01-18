e2477b53a57028934166e112f1da51b869296474
fabianlim <flqf1217@hotmail.com>
2021-01-18 21:58:54 +0800

    sign up successful

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from models.user import User
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -13,8 +13,15 @@ def new():
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    pass
+    params = request.form
+    new_user = User(username=params.get("username"), email=params.get("email"), password=params.get("password"))
 
+    if new_user.save():
+        flash("Successfully Signed Up")
+        return redirect(url_for("home"))
+    else:
+        flash(new_user.errors)
+        return redirect(url_for("users.new"))
 
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
--- models/user.py
+++ models/user.py
@@ -1,6 +1,37 @@
 from models.base_model import BaseModel
 import peewee as pw
-
+from werkzeug.security import generate_password_hash
+import re
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True, null=False)
+    email = pw.CharField(unique = True)    
+    password_hash = pw.TextField(null=False)
+    password = None
+
+    def validate(self):
+        # check if email is unique
+        existing_user_email = User.get_or_none(User.email == self.email)
+        if existing_user_email:
+            self.errors.append(f"User with {self.email} already exists.")
+        
+        # check if username is unique
+        existing_user_username = User.get_or_none(User.username == self.username)
+        if existing_user_username:
+            self.errors.append(f"User with {self.username} already exists.")
+
+        # Password Validation
+            # check if password contains more than 6
+        if len(self.password) <= 6:
+            self.errors.append("Password is less than 6 characters")
+            
+            # lower, uppercase
+        has_lower = re.search(r"[a-z]", self.password)
+        has_upper = re.search(r"[A-Z]", self.password)
+        has_special = re.search(r"[\[ \] \! \@ \# \$ \% \^ \& \* \( \)]", self.password)
+
+        if has_lower and has_upper and has_special:
+            self.password_hash = generate_password_hash(self.password)
+        else:
+            self.errors.append("Password should contain lowercase, uppercase and special characters")
+         

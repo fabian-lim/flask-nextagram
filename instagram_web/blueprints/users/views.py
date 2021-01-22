4179ea352d147eae14f48d023f05e401758b28fa
fabianlim <flqf1217@hotmail.com>
2021-01-22 17:39:40 +0800

    navbar and edit user details done

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -2,10 +2,11 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from models.user import User
 from flask_login import login_required, login_user, current_user
 
+
+
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
-
 
 @users_blueprint.route('/new', methods=['GET'])
 def new():
@@ -15,18 +16,20 @@ def new():
 @users_blueprint.route('/', methods=['POST'])
 def create():
     params = request.form
-    new_user = User(username=params.get("username"), email=params.get("email"), password=params.get("password"))
+
+    new_user = User(username = params.get("username"), email=params.get("email"), password=params.get("password"))
 
     if new_user.save():
-        flash("Successfully Signed Up")
-        return redirect(url_for("home"))
+        flash("Sign Up Successful", 'success')
+        login_user(new_user) #login new user after sign up
+        return redirect(url_for('users.show', username=new_user.username)) # redirect user to profile page
     else:
         flash(new_user.errors)
         return redirect(url_for("users.new"))
 
 @users_blueprint.route('/<username>', methods=["GET"])
 @login_required
-def show(username): 
+def show(username):
     user = User.get_or_none(User.username == username)
     if user:
         return render_template("users/show.html", user=user)
@@ -40,7 +43,8 @@ def index():
     return "USERS"
 
 
-@users_blueprint.route('/<id>/edit', methods=['GET'])
+@users_blueprint.route('/<id>/edit', methods=['GET', 'POST'])
+@login_required
 def edit(id):
     user = User.get_or_none(User.id == id)
     if user:
@@ -51,9 +55,11 @@ def edit(id):
             return redirect(url_for('users.show', username=user.username))
     else:
         flash("No user found")
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
+
 
 @users_blueprint.route('/<id>', methods=['POST'])
+@login_required
 def update(id):
     user = User.get_or_none(User.id == id)
     if user:
@@ -62,21 +68,23 @@ def update(id):
 
             user.username = params.get("username")
             user.email = params.get("email")
-            print(user.email)
-            new_password = params.get("password")
-
-            if len(new_password) > 0:
-                user.password = new_password
+           
+            password = params.get("password")
+            
+            if len(password) > 0:
+                user.password = password
             
             if user.save():
                 flash("Successfully updated details.")
-                return redirect(url_for('users.show', username=user.username))
+                return redirect(url_for("users.show", username=user.username))
             else:
-                flash("Faild to edit details. Try again.")
-                return redirect(url_for('users.edit'))
+                flash("Failed to edit the details. Try again")
+                for err in user.errors:
+                    flash(err)
+                return redirect(url_for("users.edit", id=user.id))
         else:
-            flash("You cannot edit details of another user.")
-            return redirect(url_for('home'))
+            flash("You cannot edit details of another user")
+            return redirect(url_for("home"))
     else:
         flash("No such user found")
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
--- models/user.py
+++ models/user.py
@@ -13,26 +13,25 @@ class User(UserMixin, BaseModel):
     def validate(self):
         # check if email is unique
         existing_user_email = User.get_or_none(User.email == self.email)
-        if existing_user_email:
-            self.errors.append(f"User with {self.email} already exists.")
+        if existing_user_email and existing_user_email.id !=self.id:
+            self.errors.append(f"User with {self.email} already exists")
         
-        # check if username is unique
+        #username should be unique
         existing_user_username = User.get_or_none(User.username == self.username)
-        if existing_user_username:
-            self.errors.append(f"User with {self.username} already exists.")
+        if existing_user_username and existing_user_username.id != self.id:
+            self.errors.append(f"User with {self.username} already exists")
 
-        # Password Validation
-            # check if password contains more than 6
-        if len(self.password) <= 6:
-            self.errors.append("Password is less than 6 characters")
-            
-            # lower, uppercase
-        has_lower = re.search(r"[a-z]", self.password)
-        has_upper = re.search(r"[A-Z]", self.password)
-        has_special = re.search(r"[\[ \] \! \@ \# \$ \% \^ \& \* \( \)]", self.password)
+        # Password validations
+        if self.password:
+            if len(self.password) <= 6:
+                self.errors.append("Password is less than 6 characters")
+            # lowercase characters & uppercase characters
+            has_lower = re.search(r"[a-z]", self.password)
+            has_upper = re.search(r"[A-Z]", self.password)
+            has_special = re.search(r"[\[ \] \@ \$ \* \^ \# \%]", self.password)
 
-        if has_lower and has_upper and has_special:
-            self.password_hash = generate_password_hash(self.password)
-        else:
-            self.errors.append("Password should contain lowercase, uppercase and special characters")
-         
+            if has_lower and has_upper and has_special:
+                self.password_hash = generate_password_hash(self.password)
+            else:
+                self.errors.append("Password either does not have lower, upper, or special characters")
+    
